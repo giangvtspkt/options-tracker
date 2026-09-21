0a127a483663588f774218c85f7ab18a4c97b503
giangvtspkt <giangvtspkt@gmail.com>
2026-09-21 16:06:02 +0700

    Update Twilio sender number to new default

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ GITHUB_FILE_PATH = os.getenv("GITHUB_FILE_PATH", "positions.json")
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 # Must default to the Twilio Sandbox sender number
-TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER", "whatsapp:+14155238886") 
+TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER", "whatsapp:+17372508034") 
 
 CACHE_FILE_PATH = os.path.join(tempfile.gettempdir(), "options_cache_data.json")
 
