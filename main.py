99af8e54b56725182fc1e3c07d770f956a02fa81
giangvtspkt <giangvtspkt@gmail.com>
2026-09-21 15:39:37 +0700

    Update default Twilio from number

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ GITHUB_FILE_PATH = os.getenv("GITHUB_FILE_PATH", "positions.json")
 # --- WhatsApp / Twilio Config ---
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER", "whatsapp:+14155238886") 
+TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER", "whatsapp:+17372508034") 
 
 CACHE_FILE_PATH = os.path.join(tempfile.gettempdir(), "options_cache_data.json")
 
