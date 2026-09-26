d76e28a8ff31e5706b6a0af1569048b4c4806708
giangvtspkt <giangvtspkt@gmail.com>
2026-09-26 21:31:15 +0700

    Update proxy URL for webshare session

--- main.py
+++ main.py
@@ -1460,7 +1460,7 @@ def get_options_data(tickers: str = "IREN, RKLB, AMD", contract_tickers: str = "
 
     # --- WEBSHARE PROXY SESSION SETUP ---
     yf_session = requests.Session()
-    proxy_url = "http://ehgblhyh:orsh04zky31o@198.23.243.226:6361"
+    proxy_url = "http://ehgblhyh:orsh04zky31o@198.46.161.42:5092"
     yf_session.proxies.update({
         "http": proxy_url,
         "https": proxy_url
